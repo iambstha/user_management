e71bd97b71600b7a5b9c9b7de86ebd49fbd788b5
iambstha <iambstha@gmail.com>
2023-06-07 15:45:57 +0545

    Added a newslater

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="">
       <h1>Hello again</h1>
+      <iframe src="https://reactwithbishal.substack.com/embed" width="100%" height="auto" className=' bg-white '  frameborder="0" ></iframe>
     </main>
   )
 }
